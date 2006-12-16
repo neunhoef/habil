be99a1ee078ccfdfe796e66fdfeba7651dda58a0
neunhoef <neunhoef@a5b57c73-a120-0410-9b67-99b84e0b2f6f>
2006-12-16 09:01:39 +0000

    Snapshot.
    git-svn-id: file:///scratch/neunhoef/tmp/habil/habil/trunk/habil@31 a5b57c73-a120-0410-9b67-99b84e0b2f6f

--- plotF2.py
+++ plotF2.py
@@ -3,14 +3,14 @@ from pyx import *
 g = graph.graphxy(width = 14,x=graph.axis.linear(min=0,max=60000),
                              y=graph.axis.linear(min=0,max=2600000))
 g.plot(graph.data.file("matmulF2.dat",x=1,y=2))
-g.plot(graph.data.function("y=x**3/108399995.98518534"))
+g.plot(graph.data.function("y(x)=x**3/108399995.98518534"))
 g.plot(graph.data.file("matmulF2.dat",x="2*$1",y="7*$2"))
 g.plot(graph.data.file("matmulF2.dat",x="2*$1",y="8*$2"))
 g.writeEPSfile("matmulF2_all")
 g = graph.graphxy(width = 14,x=graph.axis.linear(min=0,max=30000),
                              y=graph.axis.linear(min=0,max=300000))
 g.plot(graph.data.file("matmulF2.dat",x=1,y=2))
-g.plot(graph.data.function("y=x**3/108399995.98518534"))
+g.plot(graph.data.function("y(x)=x**3/108399995.98518534"))
 g.plot(graph.data.file("matmulF2.dat",x="2*$1",y="7*$2"))
 g.plot(graph.data.file("matmulF2.dat",x="2*$1",y="8*$2"))
 g.writeEPSfile("matmulF2_low")
