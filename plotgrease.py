13cf13e9bd1b80c861748856a6e16144e2ab1eee
neunhoef <neunhoef@a5b57c73-a120-0410-9b67-99b84e0b2f6f>
2007-01-05 00:51:16 +0000

    Transport.
    git-svn-id: file:///scratch/neunhoef/tmp/habil/habil/trunk/habil@35 a5b57c73-a120-0410-9b67-99b84e0b2f6f

---
+++ plotgrease.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2.4
+from pyx import *
+g = graph.graphxy(width = 14,x=graph.axis.linear(min=0,max=100),
+                             y=graph.axis.linear(min=0,max=50))
+ct = {"q":2.0,"d":1.0,"f1":1,"f2":1}
+
+g.plot(graph.data.function("y(x)=x*(q-1)/q*(1+1)",
+                           context = ct))
+for l in range(1,10):
+    ct["l"] = l*1.0
+    g.plot(graph.data.function("y(x) = q**l / l - d + x/l + (d-1)*f1",
+                               context = ct))
+g.writeEPSfile("guck.eps")
