9a40832e21f61b3a99227969d9345493c5f5426e
neunhoef <neunhoef@a5b57c73-a120-0410-9b67-99b84e0b2f6f>
2007-01-08 00:15:07 +0000

    Transport.
    git-svn-id: file:///scratch/neunhoef/tmp/habil/habil/trunk/habil@37 a5b57c73-a120-0410-9b67-99b84e0b2f6f

--- plotF243.py
+++ plotF243.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python2.4
 from pyx import *
-g = graph.graphxy(width = 14,x=graph.axis.linear(min=0,max=20000),
-                             y=graph.axis.linear(min=0,max=500000))
+g = graph.graphxy(width = 14,x=graph.axis.linear(min=0,max=10000),
+                             y=graph.axis.linear(min=0,max=2300000))
 g.plot(graph.data.file("matmulF243.dat",x=1,y=2))
-g.plot(graph.data.function("y(x)=x**3/23306152.532941788"))
-g.plot(graph.data.file("matmulF7.dat",x="2*$1",y="7*$2"))
-g.plot(graph.data.file("matmulF7.dat",x="2*$1",y="8*$2"))
+g.plot(graph.data.function("y(x)=x**3/444555.0892666619"))
+g.plot(graph.data.file("matmulF243.dat",x="2*$1",y="7*$2"))
+g.plot(graph.data.file("matmulF243.dat",x="2*$1",y="8*$2"))
 g.writeEPSfile("matmulF243_all")
