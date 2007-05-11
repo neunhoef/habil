fff9274969a4935c86eff044818e8ba9d1eed15a
neunhoef <neunhoef@a5b57c73-a120-0410-9b67-99b84e0b2f6f>
2007-05-11 08:58:03 +0000

    New plots and new text to describe Winograd well.
    git-svn-id: file:///scratch/neunhoef/tmp/habil/habil/trunk/habil@40 a5b57c73-a120-0410-9b67-99b84e0b2f6f

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ habil.pdf:	habil.dvi
 	dvipdfm habil
 
 habil.dvi:	*.tex matmulF2_all.eps matmulF7_all.eps matmulF243_all.eps \
-		grease.pstex_t matmulF2grease.eps
+		matmulF17_all.eps grease.pstex_t matmulF2grease.eps
 	texer
 
 clean:
@@ -25,6 +25,9 @@ matmulF243_all.eps:	matmulF243.dat plotF243.py
 
 matmulF2grease.eps:	matmulF2grease.dat plotF2grease.py
 	plotF2grease.py
+
+matmulF17_all.eps:	matmulF17.dat plotF17.py
+	plotF17.py
 
 grease.pstex_t:	grease.fig
 	fig2dev -L pstex grease.fig grease.pstex
---
+++ plotF17.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2.4
+from pyx import *
+g = graph.graphxy(width = 14,x=graph.axis.linear(min=0,max=2000),
+                             y=graph.axis.linear(min=0,max=5000))
+g.plot(graph.data.file("matmulF17.dat",x=1,y=2))
+g.plot(graph.data.function("y(x)=x**3/1702127.6595744682"))
+g.plot(graph.data.file("matmulF17.dat",x="2*$1",y="7*$2"))
+g.plot(graph.data.file("matmulF17.dat",x="2*$1",y="8*$2"))
+g.writeEPSfile("matmulF17_all")
+g = graph.graphxy(width = 14,x=graph.axis.linear(min=0,max=1000),
+                             y=graph.axis.linear(min=0,max=700))
+g.plot(graph.data.file("matmulF17.dat",x=1,y=2))
+g.plot(graph.data.function("y(x)=x**3/1702127.6595744682"))
+g.plot(graph.data.file("matmulF17.dat",x="2*$1",y="7*$2"))
+g.plot(graph.data.file("matmulF17.dat",x="2*$1",y="8*$2"))
+g.writeEPSfile("matmulF17_low")
+
